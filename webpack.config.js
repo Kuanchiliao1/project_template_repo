336b9a3ad0731d632522b9b31b1aece317152595
kuanchiliao1 <kuanchiliao@gmail.com>
2023-06-13 13:30:16 -0700

    Eslint suggested comma

--- webpack.config.js
+++ webpack.config.js
@@ -25,7 +25,7 @@ module.exports = {
           loader: 'babel-loader',
           options: {
             presets: [['@babel/preset-env', { targets: 'defaults' }]],
-            plugins: ['@babel/plugin-proposal-class-properties']
+            plugins: ['@babel/plugin-proposal-class-properties'],
           },
         },
       },
