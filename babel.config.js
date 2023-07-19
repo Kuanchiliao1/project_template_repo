923ee87b3aaa5d3bcfdbe598ce06edd167afc23d
kuanchiliao1 <kuanchiliao@gmail.com>
2023-07-19 12:22:59 -0700

    Enable Jest

--- babel.config.js
+++ babel.config.js
@@ -1,3 +1,10 @@
 module.exports = {
   presets: [['@babel/preset-env', { targets: { node: 'current' } }]],
+  plugins: ['@babel/plugin-syntax-dynamic-import'],
+
+  env: {
+    test: {
+      plugins: ['@babel/plugin-syntax-dynamic-import'],
+    },
+  },
 };
---
+++ src/tests/index.test.js
@@ -0,0 +1,3 @@
+const myFunc = require('../index');
+
+it('stuff here', () => expect(myFunc()).toBe('test'));
